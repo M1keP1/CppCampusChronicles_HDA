4605571f600304fc5bcb64332936abe78786e2e4
M1keP1 <mihirkumar654@gmail.com>
2024-02-25 23:38:35 +0100

    Initial commit Hospital software
    Added software for patient database management

---
+++ Krankenhaus/Krankenhaus/Patient_F.cpp
@@ -0,0 +1,116 @@
+#include"Patient_H.h"
+
+string Patient::get_vorname()
+{
+    return vorname;
+}
+string Patient::get_nachname()
+{
+    return nachname;
+}
+string Patient::get_geschlecht()
+{
+    return geschlecht;
+}
+int Patient::set_dauer(diagnose diagnosetyp)
+{
+    switch (diagnosetyp) {
+    case diagnose::simulant:
+        dauer = static_cast<int>(diagnose::simulant);
+        break;
+    case diagnose::infektion:
+        dauer = static_cast<int>(diagnose::infektion);
+        break;
+    case diagnose::fraktur:
+        dauer = static_cast<int>(diagnose::fraktur);
+        break;
+    case diagnose::geheilt:
+        dauer = static_cast<int>(diagnose::geheilt);
+        break;
+    default:
+        std::cout << "Unknown diagnosis." << std::endl;
+        break;
+    }
+
+    return dauer;
+}
+int Patient::get_dauer()
+{
+    return dauer;
+}
+void Patient::set_new_dauer(int newdauer)
+{
+    dauer=newdauer;
+}
+
+void Patient::set_diagnostyp(diagnose newdiagnose)
+{
+    diagnosetyp=newdiagnose;
+}
+
+string Patient::get_diagnosis_name()
+{
+    switch (diagnosetyp) {
+    case diagnose::simulant:
+        return "Simulant";
+    case diagnose::infektion:
+        return "Infektion";
+    case diagnose::fraktur:
+        return "Fraktur";
+    case diagnose::geheilt:
+        return "Geheilt";
+    default:
+        return "Unknown diagnosis";
+    }
+}
+
+
+void Patient::display()
+{
+    cout<<get_vorname();
+    cout<<get_nachname();
+    cout<<get_geschlecht();
+    cout<<get_diagnosis_name();
+
+}
+int Patient:: last_id=100000;
+
+int Krankenhaus:: get_bett()
+{
+    return bett;
+}
+
+void Krankenhaus:: addPatient(Patient pn)
+{
+    if(get_bett()!=0)
+    {
+        patients.push_back(pn);
+    }
+
+}
+
+void Krankenhaus::removePatient()
+{
+    int pos{};
+    for(auto& p2:patients)
+    {
+
+        if(p2.get_dauer()==0)
+        {
+            patients.erase(patients.begin()+pos);
+        }
+        pos++;
+    }
+}
+void Krankenhaus:: time(vector<Patient> p0,int tagen_verpasst)
+{
+    for(auto& p1:p0)
+    {
+        if(p1.get_dauer()-tagen_verpasst<=0)
+        {
+            p1.set_diagnostyp(diagnose::geheilt);
+        }
+    }
+
+}
+
---
+++ Krankenhaus/Krankenhaus/Patient_H.h
@@ -0,0 +1,77 @@
+#ifndef PATIENT_H_H
+#define PATIENT_H_H
+#include<iostream>
+#include<vector>
+using namespace std;
+
+enum class diagnose{
+    simulant=2,
+    infektion=14,
+    fraktur=72,
+    geheilt=0
+};
+class Patient{
+
+private:
+    string vorname{};
+    string nachname{};
+    string geschlecht{};
+    string geburt{};
+    int dauer{};
+
+    diagnose diagnosetyp;
+    static int last_id;
+    int id{};
+
+
+
+public:
+    Patient();
+    Patient(string vorname,string nachname,diagnose simulant):vorname(vorname),nachname(nachname),diagnosetyp(simulant){
+        last_id++;
+        id=last_id;
+    }
+
+    void display();
+    string get_vorname();
+    string get_nachname();
+    string get_geschlecht();
+    void set_diagnostyp(diagnose newdiagnose);
+    void set_new_dauer(int newdauer);
+    int get_dauer();
+    int set_dauer(diagnose diagnosetyp);
+    static int get_last_id()
+    {
+        return last_id;
+    }
+    int get_id()
+    {
+        return id;
+    }
+    string get_diagnosis_name();
+
+
+
+};
+
+
+class Krankenhaus
+{
+private:
+    int bett{};
+    vector<Patient> patients;
+    int healed_patients();
+    int krank_patients();
+
+
+public:
+
+    void addPatient(Patient pn);
+    Patient get_patients();
+    void time(vector<Patient>p0,int tagen_verpasst);
+    void removePatient();
+    int get_bett();
+    void diagnos_updater();
+};
+
+#endif // PATIENT_H_H
